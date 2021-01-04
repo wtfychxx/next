7c70b220eefcff10e7b579fbd6b5bc580be9d187
wtfychxx <fadhliyulyanto@gmail.com>
2021-01-04 16:35:44 +0700

    tailwindcssmantap

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <p>[ Hello this is my first project with nextjs framework ]</p>
         <p>
         (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <Link href="/admin/desktop"><a>our Next.js tutorial</a></Link>.)
         </p>
       </section>
     </Layout>
